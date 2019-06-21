f45c265d220b0323bfb49e6dc89631940705e902
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-06-21 23:20:28 +0530

    server side support for clean urls

--- components/PostLink.js
+++ components/PostLink.js
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 
 const PostLink = (props)=>(
-<li>
-<Link as={`/post/${props.id}`} href={`/post?title=${props.title}`}>
+<li> 
+<Link as={`/pid/${props.id}`} href={`/post?title=${props.title}`}>
 <a>{props.title}</a>
 </Link>
 </li>
